b85b4fa2cce2dc4c961d8f898c6d62c57b104648
Deibson Cogo <deibsoncogo@outlook.com>
2024-08-22 22:13:22 -0300

    feat: criação visual do component tasks e button

---
+++ src/components/Button.jsx
@@ -0,0 +1,20 @@
+export const Button = ({ children, variant = "primary" }) => {
+  const getVariantClasses = () => {
+    if (variant === "primary") {
+      return "bg-[#00adb5] text-white"
+    }
+
+    if (variant === "ghost") {
+      return "bg-transparent text-[#818181]"
+    }
+  }
+
+  return (
+    <button
+      type="button"
+      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-75 ${getVariantClasses()}`}
+    >
+      {children}
+    </button>
+  )
+}
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,29 @@
+import AddIcon from "../assets/icons/add.svg?react"
+import TrashIcon from "../assets/icons/trash.svg?react"
+import { Button } from "./Button"
+
+export const Tasks = () => {
+  return (
+    <div className="w-full px-8 py-16">
+      <div className="flex w-full justify-between">
+        <div>
+          <span className="text-xs font-semibold text-[#00adb5]">
+            Minhas Tarefas
+          </span>
+
+          <h2 className="text-xl font-semibold">Minhas Tarefas</h2>
+        </div>
+
+        <div className="flex items-center gap-3">
+          <Button variant="ghost">
+            Limpar Tarefas <TrashIcon />
+          </Button>
+
+          <Button>
+            Nova Tarefa <AddIcon />
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
