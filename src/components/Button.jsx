b21dd860588744f2d4ffb34b473863824c4bd2ee
Deibson Cogo <deibsoncogo@outlook.com>
2024-08-25 12:21:58 -0300

    feat: criação de uma nova variant do component button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,17 @@
-export const Button = ({ children, variant = "primary", ...rest }) => {
+export const Button = ({
+  children,
+  variant = "primary",
+  size = "small",
+  className,
+  ...rest
+}) => {
   const getVariantClasses = () => {
     if (variant === "primary") {
       return "bg-[#00adb5] text-white"
+    }
+
+    if (variant === "secondary") {
+      return "bg-[#eeeeee] text-[#35383e]"
     }
 
     if (variant === "ghost") {
@@ -9,10 +19,20 @@ export const Button = ({ children, variant = "primary", ...rest }) => {
     }
   }
 
+  const getSizeClasses = () => {
+    if (size === "small") {
+      return "py-1 text-xs"
+    }
+
+    if (size === "large") {
+      return "py-2 text-sm"
+    }
+  }
+
   return (
     <button
       type="button"
-      className={`flex items-center gap-2 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-75 ${getVariantClasses()}`}
+      className={`flex items-center justify-center gap-2 rounded-md px-3 font-semibold transition hover:opacity-75 ${className} ${getVariantClasses()} ${getSizeClasses()}`}
       {...rest}
     >
       {children}
