2493096597edbce4994966864f0a8f8f43d1b59c
Deibson Cogo <deibsoncogo@outlook.com>
2024-10-07 22:47:56 -0300

    feat: implementação da dependência tanstack react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import { Toaster } from "sonner"
 import App from "./App.jsx"
 import "./index.css"
 import { TaskDetails } from "./pages/TaskDetails.jsx"
-import { Toaster } from "sonner"
+
+const queryClient = new QueryClient()
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
@@ -13,7 +16,9 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Toaster toastOptions={{ style: { color: "#35383E" } }} />
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <Toaster toastOptions={{ style: { color: "#35383E" } }} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 )
